004bb1235fbf4058ff5bb539a484eba3ef385ddf
Benson-Chen-52 <bensonc52@nycstudents.net>
2024-10-14 08:36:42 -0400

    Lab 5 - Benson Chen final edition

--- Chen_Benson_Lab_5_part_3_1st_episode.cpp
+++ Chen_Benson_Lab_5_part_3_1st_episode.cpp
@@ -7,20 +7,22 @@
 using namespace std;
 
 int main()
-{
+{   
+    // Declaring varibales
     int direct;
     int north_number = 0;
     int south_number = 0;
     int east_number = 0;
     int west_number = 0;
     int steps = 0;
-    srand(time(0));
+    srand(time(0)); // Sets a random sequence of numbers based off the time
     for(int i = 1; i<=25; i++)
     {   
-        steps += 1;
+        steps += 1; //Adds 1 to steps every time the code is run.
         direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
         //1 means North, 2 means South, 3 means East and 4 means West
 
+        //Adds 1 to each of these values depending on the random number generated
         if(direct == 1)
             north_number += 1;
         else if(direct == 2)
@@ -32,18 +34,72 @@ int main()
         else
             cout << "The number generated went awire. Robot is incapacitated. Better luck next time." << endl;
 
+        // Does the calculations for each case that can happen. This one is if the north number and east number are greater than their counterparts
         if(north_number >= south_number && east_number >= west_number)
             cout << "The robot is " << north_number - south_number << " blocks North of the starting point and " 
             << east_number - west_number << " blocks East of the starting point.\n" << endl;
+        // If north number is greater, and west number is greater
         else if(north_number >= south_number && west_number > east_number)
             cout << "The robot is " << north_number - south_number<< " blocks North of the starting point and " 
             << west_number - east_number << " blocks West of the starting point.\n" << endl;
+        // If south number is greater and east number is greater
         else if(south_number > north_number && east_number >= west_number)
             cout << "The robot is " << south_number - north_number << " blocks South of the starting point and " 
             << east_number - west_number << " blocks East of the starting point.\n" << endl;
+        // If south number is greater and west number is greater
         else if(south_number > north_number && west_number > east_number)
             cout << "The robot is " << south_number - north_number << " blocks South of the starting point and " 
             << west_number - east_number << " blocks West of the starting point.\n" << endl;
     }
     return 0;
 }
+/*
+The robot is 0 blocks North of the starting point and 1 blocks East of the starting point.
+
+The robot is 0 blocks North of the starting point and 0 blocks East of the starting point.
+
+The robot is 1 blocks North of the starting point and 0 blocks East of the starting point.
+
+The robot is 0 blocks North of the starting point and 0 blocks East of the starting point.
+
+The robot is 0 blocks North of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 0 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 0 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 0 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 0 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 2 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 3 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 2 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 1 blocks South of the starting point and 1 blocks East of the starting point.
+
+The robot is 2 blocks South of the starting point and 1 blocks East of the starting point.*/
--- Chen_Benson_part_3_2nd_episode.cpp
+++ Chen_Benson_part_3_2nd_episode.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 int main()
-{
-    int direct;
+{   
+    //Declares variables
     int north_number = 0;
     int south_number = 0;
     int east_number = 0;
@@ -18,51 +18,79 @@ int main()
     int final_east;
     int final_west;
     int steps = 0;
-    int final_steps;
-    srand(time(0));
+    double final_steps = 0;
+    int real_final_north = 0;
+    int real_final_east = 0;
+    int steps_2 = 0;
+    srand(time(0)); //Generates a random sequence of numbers based off the time
     
-    for(int i = 1; i<=10; i++)
-    {
-        while(final_north != 3 || final_east != 2)
-        {   
-            steps += 1;
-            direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
-            //1 means North, 2 means South, 3 means East and 4 means West
+    while(final_north != 3 || final_east != 2) //A while loop that stops once the robot reached the destination
+    {   
+        steps += 1; // Counts the steps the robot took
+        int direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
+        //1 means North, 2 means South, 3 means East and 4 means West
 
-            if(direct == 1)
-                north_number += 1;
-            else if(direct == 2)
-                south_number += 1;
-            else if(direct == 3)
-                east_number += 1;
-            else if(direct == 4)
-                west_number += 1;  
-            else
-                cout << "The number generated went awire. Robot is incapacitated. Better luck next time." << endl;
+        // Adds 1 to each variable depending on the number generated
+        if(direct == 1)
+            north_number += 1;
+        else if(direct == 2)
+            south_number += 1;
+        else if(direct == 3)
+            east_number += 1;
+        else if(direct == 4)
+            west_number += 1;  
+        else
+            cout << "The number generated went awire. Robot is incapacitated. Better luck next time." << endl;
 
-            if(north_number >= south_number && east_number >= west_number)
-            {
-                final_north = north_number - south_number;
-                final_east = east_number - west_number;
-            }
-            else if(north_number >= south_number && west_number > east_number)
-            {
-                final_north = north_number - south_number;
-                final_west = west_number - east_number;
-            }
-            else if(south_number > north_number && east_number >= west_number)
-            {
-                final_south = south_number - north_number;
-                final_east = east_number - west_number;
-            }
-            else if(south_number > north_number && west_number > east_number)
-            {
-                final_south = south_number - north_number;
-                final_west = west_number - east_number;
-            }
-            cout << steps;
+        // Does all the calculations based off of the numbers generated.
+        // If north and east are greater than their counterparts
+        if(north_number >= south_number && east_number >= west_number)
+        {
+            final_north = north_number - south_number; // North - south
+            final_east = east_number - west_number; // East - West
+        }
+        else if(north_number >= south_number && west_number > east_number)
+        {
+            final_north = north_number - south_number; // North - South
+            final_west = west_number - east_number; // West - East
+        }
+        else if(south_number > north_number && east_number >= west_number)
+        {
+            final_south = south_number - north_number; // South - North
+            final_east = east_number - west_number; // East - West
+        }
+        else if(south_number > north_number && west_number > east_number)
+        {
+            final_south = south_number - north_number; // South - North
+            final_west = west_number - east_number; // West - East
         }
     }
-    cout << "The robot took " << steps << " step to reach 3 blocks North and 2 blocks East of the starting point.";
+    // Prints the total steps the robot took to reach the destination
+    cout << "The robot took " << steps << " steps to reach 3 blocks North and 2 blocks East of the starting point.\n";
+
+    //Runs 10 iterations of the code
+    for (int i = 1; i <= 10; i++) 
+    {   
+        // Another while loop that serves the same purpose as the first one
+        while (real_final_north != 3 || real_final_east != 2) 
+        {
+            int direction = rand() % 4 + 1;  // Random number generator
+            
+            // If statements that changes the values based off of the number generated
+            if(direction == 1) real_final_north++;      
+            else if(direction == 2) real_final_north--; 
+            else if(direction == 3) real_final_east++; 
+            else if(direction == 4) real_final_east--; 
+
+            steps_2++; // Counts the steps
+        }
+        final_steps += steps_2; // Adds the steps for each iteration
+    }
+    double average_steps = final_steps/10; // The calculations for the average steps
+    // Prints the average number of steps it took to reach the destination
+    cout << "On average the robot took " << average_steps << " to reach 3 blocks North and 2 blocks East of the starting point across 10 iterations.";
     return 0;
 }
+/*
+The robot took 105 steps to reach 3 blocks North and 2 blocks East of the starting point.
+On average the robot took 6807 to reach 3 blocks North and 2 blocks East of the starting point across 10 iterations.*/
